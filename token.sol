4590ffbbc897ad50de58743672061d51ce1b0e5a
itsvishal143 <149154061+itsvishal143@users.noreply.github.com>
2024-06-08 12:10:02 +0530

    mytoken.sol

--- token.sol
+++ token.sol
@@ -15,33 +15,33 @@ pragma solidity 0.8.18;
        to the amount that is supposed to be burned.
 */
 
-
 contract MyToken {
 
     // public variables here
-    string public tokenName = "vis";
-    string public abbrv = "vi";
-    uint public totalSupply = 0;
-    
+
+    string public tokenname = "vishal";
+    string public tokenabbrv = "vis";
+    uint public totalsupply = 0;
 
     // mapping variable here
-    mapping (address=> uint) public balances;
+
+     mapping(address => uint) public balances;
 
     // mint function
-    function mint(address _address, uint _value) public {
-        totalSupply += _value;
-        balances[_address] += _value;  
 
+    function mint(address _address, uint _value) public{
+        totalsupply += _value;
+        balances[_address] += _value;
     }
 
-
     // burn function
-    function burn( address _address,uint _value) public {
-        if( balances[_address] >= _value){
-            totalSupply -= _value;
-            balances[_address] -= _value;
-        }
-        
+
+    function burn(address _address, uint _value) public{
+        if (balances[_address] >=_value) { 
+        totalsupply -= _value;
+        balances[_address] -= _value;
+    }
     }
 
 }
+
